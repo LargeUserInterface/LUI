a4aebf3f381256f93f8f35115e3bc102410a46da
GGEveryone <henry199371@gmail.com>
2018-07-27 10:02:13 -0400

    Fill the screen with videos

--- lui/src/components/Videos/VideosApp.js
+++ lui/src/components/Videos/VideosApp.js
@@ -20,10 +20,10 @@ const styles = {
 
   frameContainer: {
     display: 'inline-block',
-    width: '25%',
+    width: '33.3%',
     verticalAlign: 'middle',
     boxSizing: 'border-box',
-    padding: '20px',
+    padding: '0px',
     position: 'relative',
     border: '2px solid #37474F',
     '&:hover': {
@@ -33,17 +33,18 @@ const styles = {
   },
 };
 
-const video1 = {
-    id: "rnlCGw-0R8g",
-}
+const videos = [
+    {id: 'rnlCGw-0R8g'},
+    {id: 'zw47_q9wbBE'},
+    {id: '7m6J8W6Ib4w'},
+    {id: 'l7uuTnk69Eo'},
+    {id: '6ZfuNTqbHE8'},
+    {id: 'mFIOGpIQtVU'},
+    {id: 'c-SE2Qeqj1g'},
+    {id: '-AbaV3nrw6E'},
+    {id: 'FTPmnZVgDjQ'}
+]
 
-const video2 = {
-    id: "zw47_q9wbBE",
-}
-
-const video3 = {
-    id: "7m6J8W6Ib4w",
-}
 
 const opts = {
   height: '315',
@@ -116,17 +117,47 @@ class VideosApp extends Component {
         return (
             <div className={classes.container}>
               <YouTube className = {classes.frameContainer}
-                videoId={video1.id}
+                videoId={videos[0].id}
                 opts={opts}
                 onReady={this._onReady}
               />
               <YouTube className = {classes.frameContainer}
-                videoId={video2.id}
+                videoId={videos[1].id}
                 opts={opts}
                 onReady={this._onReady}
               />
               <YouTube className = {classes.frameContainer}
-                videoId={video3.id}
+                videoId={videos[2].id}
+                opts={opts}
+                onReady={this._onReady}
+              />
+              <YouTube className = {classes.frameContainer}
+                videoId={videos[3].id}
+                opts={opts}
+                onReady={this._onReady}
+              />
+              <YouTube className = {classes.frameContainer}
+                videoId={videos[4].id}
+                opts={opts}
+                onReady={this._onReady}
+              />
+              <YouTube className = {classes.frameContainer}
+                videoId={videos[5].id}
+                opts={opts}
+                onReady={this._onReady}
+              />
+              <YouTube className = {classes.frameContainer}
+                videoId={videos[6].id}
+                opts={opts}
+                onReady={this._onReady}
+              />
+              <YouTube className = {classes.frameContainer}
+                videoId={videos[7].id}
+                opts={opts}
+                onReady={this._onReady}
+              />
+              <YouTube className = {classes.frameContainer}
+                videoId={videos[8].id}
                 opts={opts}
                 onReady={this._onReady}
               />
