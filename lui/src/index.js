e00e8947faa17c2f303e8f0378bbbb8733691f9a
ajlee19 <ashleyjlee19@gmail.com>
2018-08-03 23:31:58 -0700

    updated main to page

--- lui/src/index.js
+++ lui/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Pages from './Pages';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Pages />, 
+    // <Pages />, 
+    <App />,
     document.getElementById('root')
 );
 registerServiceWorker();
