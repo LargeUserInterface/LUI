917ee68180bf5df3b7aba2c43daa85e5db401437
GGEveryone <GGEveryone@users.noreply.github.com>
2018-08-08 09:32:32 -0400

    Update index.js

--- lui/src/components/Videos/index.js
+++ lui/src/components/Videos/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
 import { ButtonBase, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import VideosApp from './VideosApp';
+import classNames from 'classnames';
 
 const styles = theme => ({
     root: {
@@ -16,22 +18,20 @@ const styles = theme => ({
         height: '100%',
         [theme.breakpoints.down('xs')]: {
             width: '100% !important', // Overrides inline-style
-            // height: 100,
-        },
-        '&:hover, &$focusVisible': {
-            zIndex: 1,
-            '& $imageBackdrop': {
-                opacity: 0.15,
-            },
-            '& $imageMarked': {
-                opacity: 0,
-            },
-            '& $imageTitle': {
-                border: '4px solid currentColor',
-            },
         },
     },
-    focusVisible: {},
+    hovered: {
+        zIndex: 1,
+        '& $imageBackdrop': {
+            opacity: 0.15,
+        },
+        '& $imageMarked': {
+            opacity: 0,
+        },
+        '& $imageTitle': {
+            border: '4px solid currentColor',
+        },
+    },
     imageButton: {
         position: 'absolute',
         left: 0,
@@ -100,14 +100,18 @@ class Videos extends Component {
     }
 
     render() {
-        const { classes } = this.props;
+        const { classes, hovered, clicked } = this.props;
 
-        if (!this.state.clicked) {
+        if ( clicked ) {
+            // full app
+            return <Redirect to={{pathname: "/Videos"}} />
+        } else {
+            // icon
             return (
                 <ButtonBase
                     focusRipple
                     key={image.title}
-                    className={classes.image}
+                    className={hovered? classNames(classes.image, classes.hovered) : classes.image}
                     focusVisibleClassName={classes.focusVisible}
                     style={{
                         width: image.width,
@@ -133,12 +137,18 @@ class Videos extends Component {
                     </span>
                 </ButtonBase>
             );
-        } else {
-            return (
-                <VideosApp />
-            );
         }
     }
 }
+
+Videos.propTypes = {
+    hovered: PropTypes.bool,
+    clicked: PropTypes.bool,
+};
+
+Videos.defaultProps = {
+    hovered: false,
+    clicked: false
+};
 
 export default withStyles(styles)(Videos);
