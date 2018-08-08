aacb99ee0d9c1a0df1533b040fd21ddef0b2f141
GGEveryone <GGEveryone@users.noreply.github.com>
2018-08-08 09:33:18 -0400

    Update VideosApp.js

--- lui/src/components/Videos/VideosApp.js
+++ lui/src/components/Videos/VideosApp.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import YouTube from 'react-youtube';
-
+import Leap from './leap.js'
+import { Redirect } from 'react-router';
 
 const styles = {
   container: {
@@ -45,7 +46,6 @@ const videos = [
     {id: 'FTPmnZVgDjQ'}
 ]
 
-
 const opts = {
   height: '315',
   width: '560',
@@ -54,122 +54,162 @@ const opts = {
   }
 };
 
-
 class VideosApp extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            videos: [],
+            hovered: "",
+            clicked: "",
+            exit: false,
+        }
     }
 
-//     render() {
-//         return (
-//             <iframe id={video1.id}
-//                     width={video1.width} height={video1.height}
-//                     frameborder={video1.frameborder}
-//                     style={video1.style}
-//             ></iframe>
-//         )
-//         var tag = document.createElement('script');
-//         tag.id = 'iframe-demo';
-//         tag.src = 'https://www.youtube.com/iframe_api';
-//         var firstScriptTag = document.getElementsByTagName('script')[0];
-//         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-//
-//         var player;
-//         function onYouTubeIframeAPIReady() {
-//             player = new YT.Player('existing-iframe-example', {
-//                 events: {
-//                   'onReady': onPlayerReady,
-//                   'onStateChange': onPlayerStateChange
-//                 }
-//             });
-//         }
-//         function onPlayerReady(event) {
-//             document.getElementById('existing-iframe-example').style.borderColor = '#FF6D00';
-//         }
-//         function changeBorderColor(playerStatus) {
-//             var color;
-//             if (playerStatus == -1) {
-//               color = "#37474F"; // unstarted = gray
-//             } else if (playerStatus == 0) {
-//               color = "#FFFF00"; // ended = yellow
-//             } else if (playerStatus == 1) {
-//               color = "#33691E"; // playing = green
-//             } else if (playerStatus == 2) {
-//               color = "#DD2C00"; // paused = red
-//             } else if (playerStatus == 3) {
-//               color = "#AA00FF"; // buffering = purple
-//             } else if (playerStatus == 5) {
-//               color = "#FF6DOO"; // video cued = orange
-//             }
-//             if (color) {
-//               document.getElementById('existing-iframe-example').style.borderColor = color;
-//             }
-//         }
-//         function onPlayerStateChange(event) {
-//             changeBorderColor(event.data);
-//         }
-//     }
-// }
+    componentDidMount() {
+        this.getVideos();
+    }
+
+    getVideos = () => {
+        const videos = [this.refs.video1, this.refs.video2, this.refs.video3,
+                       this.refs.video4, this.refs.video5, this.refs.video6,
+                       this.refs.video7, this.refs.video8, this.refs.video9];
+        this.setState({videos});
+    }
+
+    handleHover = (video) => {
+        this.setState({hovered:video})
+    }
+
+    handleClick = (video) => {
+        this.setState({clicked:video})
+        // this.refs.video1.playVideo();
+    }
+
+    handleExit = () => {
+      console.log("exit");
+      this.setState({
+        exit: true
+      })
+    }
 
     render() {
         const { classes } = this.props;
+        const { hovered } = this.state;
+        const { clicked } = this.state;
+
+        if (this.state.exit) {
+          return <Redirect to={{ pathname: "/" }} />
+        }
 
         return (
             <div className={classes.container}>
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[0].id}
-                opts={opts}
-                onReady={this._onReady}
+              <Leap
+                videos={this.state.videos}
+                handleHover={this.handleHover}
+                handleSwipe={this.handleSwipe}
+                handleExit={this.handleExit}
+                handleClick={this.handleClick}
               />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[1].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[2].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[3].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[4].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[5].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[6].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[7].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-              <YouTube className = {classes.frameContainer}
-                videoId={videos[8].id}
-                opts={opts}
-                onReady={this._onReady}
-              />
-
+              <div>
+                  <YouTube ref="video1" item className = {classes.frameContainer}
+                    videoId={videos[0].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video1"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video2" item className = {classes.frameContainer}
+                    videoId={videos[1].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video2"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video3" item className = {classes.frameContainer}
+                    videoId={videos[2].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video3"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video4" item className = {classes.frameContainer}
+                    videoId={videos[3].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video4"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video5" item className = {classes.frameContainer}
+                    videoId={videos[4].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video5"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video6" item className = {classes.frameContainer}
+                    videoId={videos[5].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video6"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video7" item className = {classes.frameContainer}
+                    videoId={videos[6].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video7"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video8" item className = {classes.frameContainer}
+                    videoId={videos[7].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video8"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+                  <YouTube ref="video9" item className = {classes.frameContainer}
+                    videoId={videos[8].id}
+                    opts={opts}
+                    onReady={this._onReady}
+                    // clicked={this.state.clicked === "video9"}
+                    onStateChange={this._onStateChange}
+                    onPause={this._onPause}
+                  />
+              </div>
             </div>
         );
       }
 
       _onReady(event) {
         // access to player in all event handlers via event.target
-        event.target.pauseVideo();
+        event.target.playVideo();
+        // console.log(event.data)
+      }
+
+      _onPause(event) {
+          console.log(event.data);
+      }
+
+      _onStateChange(event) {
+          console.log("Event",event);
+          console.log("Clicked", this.state.clicked)
+          if (event.data == -1) {
+              event.target.pauseVideo();
+          }
       }
   }
+
+  VideosApp.defaultProps = {
+      hovered: false,
+      clicked: false
+  };
 
 export default withStyles(styles)(VideosApp);
