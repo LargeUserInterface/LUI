81b294ab96394b56c923d5dcbcad163e7d109ce8
ajlee19 <ashleyjlee19@gmail.com>
2018-08-11 23:50:20 -0700

    updated clickable

--- lui/src/App.js
+++ lui/src/App.js
@@ -122,7 +122,7 @@ class App extends Component {
               <Photos hovered={this.state.hovered === "card1"} clicked={this.state.clicked === "card1"} />
             </Grid>
             <Grid ref="card2" item xs={4} >
-              <Videos hovered={this.state.hovered === "card2"} clicked={false} />
+              <Videos hovered={this.state.hovered === "card2"} clicked={this.state.clicked === "card2"} />
             </Grid>
             <Grid ref="card3" item xs={4} >
               <App3 hovered={this.state.hovered === "card3"} clicked={false} />
